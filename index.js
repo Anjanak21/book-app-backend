70d931228395ecccdc703ba02254c16ec1fcd22b
Anjanak21 <anjanadamodharank004@gmail.com>
2025-02-07 12:30:50 +0530

    first commit

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require('express');
+const app = express();
+const cors = require('cors');
 const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
-require('dotenv').config()
+require('dotenv').config();
 
 app.use(express.json());
-app.use(cors({
-   
-}))
 
-const bookRoutes = require('./src/books/book.route')
+// Set up CORS with the frontend origin
+app.use(cors({
+  origin: 'https://book-app-frontend-nu.vercel.app',  // Your frontend domain
+  methods: 'GET,POST,PUT,DELETE',  // Specify allowed HTTP methods
+  allowedHeaders: 'Content-Type,Authorization',  // Specify allowed headers
+}));
+
+const bookRoutes = require('./src/books/book.route');
 const orderRoutes = require('./src/orders/order.route');
 const userRoutes = require("./src/users/user.route");
-const adminRoutes = require("./src/stats/admin.stats")
-app.use('/api/books', bookRoutes)
-app.use('/api/orders', orderRoutes)
-app.use('/api/auth', userRoutes)
-app.use("/api/admin", adminRoutes)
+const adminRoutes = require("./src/stats/admin.stats");
+
+app.use('/api/books', bookRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/auth', userRoutes);
+app.use("/api/admin", adminRoutes);
 
 async function main() {
-    await mongoose.connect(process.env.DB_URL);
-
-
-
-    app.use('/', (req, res) => {
-        res.send("Book server");
-    })
+  await mongoose.connect(process.env.DB_URL);
+  
+  app.use('/', (req, res) => {
+    res.send("Book server");
+  });
 }
 
-main().then(() => console.log("mongo connected succesfully")
-).
-    catch(err => console.log(err));
+main().then(() => console.log("MongoDB connected successfully"))
+  .catch(err => console.log(err));
 
 app.listen(port, () => {
-    console.log(`listening port ${port}`);
-
-})
-
+  console.log(`Listening on port ${port}`);
+});
