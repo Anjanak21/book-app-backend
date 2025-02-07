e42f35251a213901359cf7f487ec1fb447cb37e3
Anjanak21 <anjanadamodharank004@gmail.com>
2025-02-07 12:33:05 +0530

    first commit

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
-app.use(express.json());
-
-// Set up CORS with the frontend origin
 app.use(cors({
   origin: 'https://book-app-frontend-nu.vercel.app',  // Your frontend domain
   methods: 'GET,POST,PUT,DELETE',  // Specify allowed HTTP methods
   allowedHeaders: 'Content-Type,Authorization',  // Specify allowed headers
 }));
+app.use(express.json());
+
+// Set up CORS with the frontend origin
 
 const bookRoutes = require('./src/books/book.route');
 const orderRoutes = require('./src/orders/order.route');
