dd8e248d9b44a14ac8c0e761cf50cbb8978e6c69
Anjanak21 <anjanadamodharank004@gmail.com>
2025-02-07 12:21:29 +0530

    first commit

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ require('dotenv').config()
 
 app.use(express.json());
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:['http://localhost:5173','https://book-app-frontend-nu.vercel.app'],
     credentials:true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['*'],
 }))
 
 const bookRoutes=require('./src/books/book.route')
